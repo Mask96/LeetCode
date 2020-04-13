0b6b46845ce2438afeca012375e172431dff5ffd
Mask <781064841@qq.comm>
2020-04-13 22:20:56 +0800

    完成了逆波兰式计计算

--- src/com/company/evaluateReversePolishNotation.java
+++ src/com/company/evaluateReversePolishNotation.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.util.Stack;
+
 public class evaluateReversePolishNotation {
 
-    public static void main(String[] tokens) {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> numStack = new Stack<>();
 
+        for (int i = 0; i < tokens.length; i++) {
+           try{
+               int num = Integer.parseInt(tokens[i]);
+               numStack.push(num);
+           }catch (Exception e){
+               //如果是其他计算符号在parseInt时会异常，进入异常处理程序
+               int b = numStack.pop();
+               int a = numStack.pop();
+               numStack.push(compute(a,b,tokens[i]));
+           }
+        }
+
+        return numStack.pop();
     }
+
+    public int compute(int num1, int num2, String sym) {
+        Integer num3;
+        switch (sym) {
+            case "+":
+                num3 = num1 + num2;
+                break;
+            case "-":
+                num3 = num1 - num2;
+                break;
+            case "*":
+                num3 = num1 * num2;
+                break;
+            case "/":
+                num3 = num1 / num2;
+                break;
+            default:
+                num3 = 0;
+        }
+        return num3;
+    }
+
+
 }
