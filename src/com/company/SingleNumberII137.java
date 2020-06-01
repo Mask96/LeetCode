27a3031e0b81c0c295a9a0fd790870aa343de2db
Mask96 <781064841@qq.com>
2020-06-01 17:23:54 +0800

    137

--- src/com/company/SingleNumberII137.java
+++ src/com/company/SingleNumberII137.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.HashMap;
+
 /**
  * @description: 现在有一个整数类型的数组，数组中只有一个元素只出现一次，其余元素都出现三次。你需要找出只出现一次的元素
  * 注意：
@@ -8,7 +10,23 @@ package com.company;
  * @time: 2020/5/27 5:50 下午
  */
 public class SingleNumberII137 {
-    public int singleNumber (int[] A) {
+    public int singleNumber(int[] A) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for (int value : A) {
+            if (map.containsKey(value)) {
+                map.put(value, map.get(value) + 1);
+            } else {
+                map.put(value, 1);
+            }
+        }
+
+        for (int i : map.keySet()) {
+            if (map.get(i) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
