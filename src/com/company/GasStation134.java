7338719f5ef27b0075e8f0e1b726c883dd1e759d
Mask96 <781064841@qq.com>
2020-06-04 17:58:12 +0800

    134

--- src/com/company/GasStation134.java
+++ src/com/company/GasStation134.java
@@ -1,9 +1,5 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
 /**
  * @description: 环形路上有n个加油站，第i个加油站的汽油量是gas[i].
  * 你有一辆车，车的油箱可以无限装汽油。从加油站i走到下一个加油站（i+1）花费的油量是cost[i]，你从一个加油站出发，刚开始的时候油箱里面没有汽油。
@@ -14,13 +10,23 @@ import java.util.List;
  */
 public class GasStation134 {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // 贪心，先去最多的油站
-        int length = gas.length;
-        List<Integer> indexList;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-
+        int len = gas.length;
+        for (int i = 0; i < len; i++) {
+            // 开始遍历起点
+            int j = i;
+            int count = gas[i];
+            while (count >= cost[j]) {
+                // 现有的油还足够到下个点
+                count = count - cost[j];
+                // 下个点的index为j，因为是个圈，所以可以取余数。
+                j = (j + 1) % len;
+                count = count + gas[j];
+                // j回到了i
+                if (j == i) {
+                    return i;
+                }
             }
         }
+        return -1;
     }
 }
