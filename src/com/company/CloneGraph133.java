24d20db13c77e4855386a6f73c52db1c435f2e0d
Mask <781064841@qq.comm>
2020-06-15 22:01:56 +0800

    '133'

--- src/com/company/CloneGraph133.java
+++ src/com/company/CloneGraph133.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.*;
+
 /**
  * @description: 本题要求复制一个无向图，图中每个节点都包含一个标签和它的邻居列表
  * 我们无向图用以下的方法序列化：
@@ -13,5 +15,62 @@ package com.company;
  * @author: Mask
  * @time: 2020/6/4 5:48 下午
  */
+class UndirectedGraphNode {
+    int label;
+    ArrayList<UndirectedGraphNode> neighbors;
+
+    UndirectedGraphNode(int x) {
+        label = x;
+        neighbors = new ArrayList<UndirectedGraphNode>();
+    }
+};
+
 public class CloneGraph133 {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        Map<Integer, UndirectedGraphNode> nodeMap = new HashMap<>();
+        Queue<UndirectedGraphNode> nodeQueue = new LinkedList<>();
+        Set<Integer> nodeSet = new HashSet<>();
+
+        if(node == null){
+            return node;
+        }
+
+        // 第一次宽度遍历。将所有点存在map中。
+        nodeQueue.offer(node);
+        while (!nodeQueue.isEmpty()) {
+            // 如果队列里不为空，将第一个取出，将其邻居节点加入队列。取出点加入到map和set
+            UndirectedGraphNode curNode = nodeQueue.poll();
+
+            if (!nodeSet.contains(curNode.label)) {
+                //第一次加入该节点，set中没有
+                for (UndirectedGraphNode neiNode : curNode.neighbors) {
+                    nodeQueue.offer(neiNode);
+                }
+                UndirectedGraphNode newNode = new UndirectedGraphNode(curNode.label);
+                nodeMap.put(curNode.label, newNode);
+                nodeSet.add(curNode.label);
+            }
+        }
+
+        nodeSet.clear();
+        nodeQueue.clear();
+        // 第二次宽度遍历，加入邻居节点
+        nodeQueue.offer(node);
+        while (!nodeQueue.isEmpty()) {
+            UndirectedGraphNode curNode = nodeQueue.poll();
+
+            if (!nodeSet.contains(curNode.label)) {
+                ArrayList<UndirectedGraphNode> neiNodeList = new ArrayList<>();
+                for (UndirectedGraphNode neiNode : curNode.neighbors) {
+                    neiNodeList.add(nodeMap.get(neiNode.label));
+                    nodeQueue.offer(neiNode);
+                }
+                nodeMap.get(curNode.label).neighbors = neiNodeList;
+                nodeSet.add(curNode.label);
+            }
+
+        }
+
+        return nodeMap.get(node.label);
+    }
 }
