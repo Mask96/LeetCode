be27e5a318f1eb7a816cfc4171e1f8198a1a5eb0
Mask <781064841@qq.comm>
2020-11-16 23:13:49 +0800

    '48,49,53'

---
+++ src/com/company/hot100/A48_Rotate.java
@@ -0,0 +1,56 @@
+package com.company.hot100;
+
+/**
+ * @Classname A48_Rotate
+ * @Description 给定一个 n × n 的二维矩阵表示一个图像。
+ * <p>
+ * 将图像顺时针旋转 90 度。
+ * <p>
+ * 说明：
+ * <p>
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ] public void rotate(int[][] matrix) {
+ * <p>
+ * }
+ * @Date 2020/11/16 22:05
+ * @Created by Mask
+ */
+public class A48_Rotate {
+
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+
+//        1. 先对角线交换
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+//        2. 左右镜像交换
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][len - j - 1];
+                matrix[i][len - j - 1] = temp;
+            }
+        }
+    }
+}
